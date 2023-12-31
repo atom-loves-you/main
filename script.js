22dd083befbc0791aff07df5fb0b5aa362da14f1
kschenk10-ivy-tech <atom.loves.you.2022@gmail.com>
2023-12-31 15:59:57 -0800

    Update script.js

--- script.js
+++ script.js
@@ -39,41 +39,41 @@ Amplitude.init({
   },
   songs: [
     {
-      name: "The Call of the Void",
+      name: "Track 01: Ad Astra",
       artist: "Atom Loves You",
-      album: "L'appel du vide",
+      album: "Ad Astra per Aspera, 2023",
       url:
         "https://assets.codepen.io/6566924/Ad+Astra.mp3",
       cover_art_url: "https://assets.codepen.io/6566924/bright-colors-grid-square.png"
     },
     {
-      name: "Tweeters, Woofers, Heavy Metal",
+      name: "Track 02: Tweeters, Woofers, Heavy Metal",
       artist: "Atom Loves You",
-      album: "L'appel du vide",
+      album: "Ad Astra per Aspera, 2023",
       url:
         "https://github.com/atom-loves-you/The-Call-of-the-Void/blob/main/Tweeters-Woofers-Heavy-Metal.mp3?raw=true",
       cover_art_url: "https://assets.codepen.io/6566924/bright-colors-grid-square.png"
     },
     {
-      name: "Bitwise (Three)",
+      name: "Track 03: 143 Nao",
       artist: "Atom Loves You",
-      album: "L'appel du vide",
+      album: "Ad Astra per Aspera, 2023",
       url:
-        "https://github.com/atom-loves-you/The-Call-of-the-Void/blob/main/m4a/Call%20of%20the%20Void%20.m4a?raw=true",
+        "https://github.com/atom-loves-you/Ad-Astra/blob/main/Ad-Astra-2024/143%20Nao.wav",
       cover_art_url: "https://assets.codepen.io/6566924/bright-colors-grid-square.png"
     },
     {
-      name: "Dusk To Dawn",
+      name: "Track 04:Dusk To Dawn",
       artist: "Atom Loves You",
-      album: "L'appel du vide",
+      album: "Ad Astra per Aspera, 2023",
       url:
         "https://github.com/atom-loves-you/The-Call-of-the-Void/blob/main/Call-of-the-Void.mp3?raw=true",
       cover_art_url: "https://assets.codepen.io/6566924/bright-colors-grid-square.png"
     },
     {
-      name: "Anthem",
+      name: "Track 05: Anthem",
       artist: "Atom Loves You",
-      album: "L'appel du vide",
+      album: "Ad Astra per Aspera, 2023",
       url:
         "https://github.com/atom-loves-you/samples/blob/main/bass-samples/Funky-Drill-n-Bass-Loop.wav?raw=true",
       cover_art_url: "https://assets.codepen.io/6566924/bright-colors-grid-square.png"
