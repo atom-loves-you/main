666da67f34395dbff14ccc327c77bf59a82a83d4
kschenk10-ivy-tech <atom.loves.you.2022@gmail.com>
2023-12-31 15:38:53 -0800

    Updates

---
+++ Docs/README.md
@@ -0,0 +1,7 @@
+#  Atom Loves You Tailwind 3.0 Audio Player
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/Khristin-Schenk/pen/poOZdmG/df4db54120f632b6e48608636de44162](https://codepen.io/Khristin-Schenk/pen/poOZdmG/df4db54120f632b6e48608636de44162).
+
+Saw this amazing player design by Steve Schoger (https://twitter.com/steveschoger/status/1499780794193166341?s=20&t=SC8akXhJOIdDj4AjnASuAA) and asked if we could use it as an example for AmplitudeJS. He was super gracious and let us make a sample player. It's a perfect use case for AmplitudeJS showing the power of the functionality along with the flexibility of different designs.
+
+The player is built with the newest version of TailwindCSS v3.0 and AmplitudeJS 5.2
---
+++ script.js
@@ -0,0 +1,86 @@
+let theme = window.matchMedia("(prefers-color-scheme: dark)").matches
+  ? "dark"
+  : "light";
+
+if (theme == "dark") {
+  document.documentElement.classList.add("dark");
+}
+
+document
+  .getElementById("dark-mode-toggle")
+  .addEventListener("click", function () {
+    document.documentElement.classList.toggle("dark");
+  });
+
+document.getElementById("song-saved").addEventListener("click", function () {
+  document.getElementById("song-saved").classList.toggle("saved");
+});
+
+Amplitude.init({
+  bindings: {
+    37: "prev",
+    39: "next",
+    32: "play_pause"
+  },
+  callbacks: {
+    timeupdate: function () {
+      let percentage = Amplitude.getSongPlayedPercentage();
+
+      if (isNaN(percentage)) {
+        percentage = 0;
+      }
+
+      /**
+       * Massive Help from: https://nikitahl.com/style-range-input-css
+       */
+      let slider = document.getElementById("song-percentage-played");
+      slider.style.backgroundSize = percentage + "% 100%";
+    }
+  },
+  songs: [
+    {
+      name: "The Call of the Void",
+      artist: "Atom Loves You",
+      album: "L'appel du vide",
+      url:
+        "https://github.com/atom-loves-you/The-Call-of-the-Void/blob/main/m4a/Call%20of%20the%20Void%20-%20Three.m4a?raw=true",
+      cover_art_url: "https://assets.codepen.io/6566924/orange-star.svg"
+    },
+    {
+      name: "Tweeters, Woofers, Heavy Metal",
+      artist: "Atom Loves You",
+      album: "L'appel du vide",
+      url:
+        "https://github.com/atom-loves-you/The-Call-of-the-Void/blob/main/Tweeters-Woofers-Heavy-Metal.mp3?raw=true",
+      cover_art_url: "https://assets.codepen.io/6566924/orange-star.svg"
+    },
+    {
+      name: "Bitwise (Three)",
+      artist: "Atom Loves You",
+      album: "L'appel du vide",
+      url:
+        "https://github.com/atom-loves-you/The-Call-of-the-Void/blob/main/m4a/Call%20of%20the%20Void%20.m4a?raw=true",
+      cover_art_url: "https://assets.codepen.io/6566924/orange-star.svg"
+    },
+    {
+      name: "Dusk To Dawn",
+      artist: "Atom Loves You",
+      album: "L'appel du vide",
+      url:
+        "https://github.com/atom-loves-you/The-Call-of-the-Void/blob/main/Call-of-the-Void.mp3?raw=true",
+      cover_art_url: "https://assets.codepen.io/6566924/orange-star.svg"
+    },
+    {
+      name: "Anthem",
+      artist: "Atom Loves You",
+      album: "L'appel du vide",
+      url:
+        "https://github.com/atom-loves-you/samples/blob/main/bass-samples/Funky-Drill-n-Bass-Loop.wav?raw=true",
+      cover_art_url: "https://assets.codepen.io/6566924/orange-star.svg"
+    }
+  ]
+});
+
+window.onkeydown = function (e) {
+  return !(e.keyCode == 32);
+};
