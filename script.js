660edc4c35e0c0ff8d4c3e51e654fa181feffed7
kschenk10-ivy-tech <atom.loves.you.2022@gmail.com>
2023-12-31 16:24:14 -0800

    Update script.js

--- script.js
+++ script.js
@@ -47,39 +47,74 @@ Amplitude.init({
       cover_art_url: "https://assets.codepen.io/6566924/bright-colors-grid-square.png"
     },
     {
-      name: "Track 02: Tweeters, Woofers, Heavy Metal",
+      name: "Track 02: Ad Astra 1 (un-named)",
       artist: "Atom Loves You",
       album: "Ad Astra per Aspera, 2023",
       url:
-        "https://github.com/atom-loves-you/The-Call-of-the-Void/blob/main/Tweeters-Woofers-Heavy-Metal.mp3?raw=true",
+        "https://github.com/atom-loves-you/main/blob/main/mp3/Ad%20Astra%201.mp3",
       cover_art_url: "https://assets.codepen.io/6566924/bright-colors-grid-square.png"
     },
     {
-      name: "Track 03: 143 Nao",
+      name: "Track 03: Tweeters, Woofers, Heavy Metal",
       artist: "Atom Loves You",
       album: "Ad Astra per Aspera, 2023",
       url:
-        "https://github.com/atom-loves-you/Ad-Astra/blob/main/Ad-Astra-2024/143%20Nao.wav",
+        "https://github.com/atom-loves-you/main/blob/main/mp3/Tweeters%2C%20Woofers%2C%20Heavy%20Metal%20(quick).mp3",
       cover_art_url: "https://assets.codepen.io/6566924/bright-colors-grid-square.png"
     },
     {
-      name: "Track 04:Dusk To Dawn",
+      name: "Track 04: Otherkin",
       artist: "Atom Loves You",
       album: "Ad Astra per Aspera, 2023",
       url:
-        "https://github.com/atom-loves-you/The-Call-of-the-Void/blob/main/Call-of-the-Void.mp3?raw=true",
+        "https://github.com/atom-loves-you/main/blob/main/mp3/Otherkin.mp3",
       cover_art_url: "https://assets.codepen.io/6566924/bright-colors-grid-square.png"
     },
     {
-      name: "Track 05: Anthem",
+      name: "Track 05: Ad-6 (unnamed)",
       artist: "Atom Loves You",
       album: "Ad Astra per Aspera, 2023",
       url:
-        "https://github.com/atom-loves-you/samples/blob/main/bass-samples/Funky-Drill-n-Bass-Loop.wav?raw=true",
+        "https://github.com/atom-loves-you/main/blob/main/mp3/Ad%206.mp3",
+      cover_art_url: "https://assets.codepen.io/6566924/bright-colors-grid-square.png"
+    },
+    {
+      name: "Track 06: H=h2p",
+      artist: "Atom Loves You",
+      album: "Ad Astra per Aspera, 2023",
+      url:
+        "https://github.com/atom-loves-you/main/blob/main/mp3/H%3Dh2pi.mp3",
+      cover_art_url: "https://assets.codepen.io/6566924/bright-colors-grid-square.png"
+    },
+    {
+      name: "Track 07: Ooo",
+      artist: "Atom Loves You",
+      album: "Ad Astra per Aspera, 2023",
+      url:
+        "https://github.com/atom-loves-you/main/blob/main/mp3/Ooo.mp3",
+      cover_art_url: "https://assets.codepen.io/6566924/bright-colors-grid-square.png"
+    },
+    {
+      name: "Track 08: 143 Nao",
+      artist: "Atom Loves You",
+      album: "Ad Astra per Aspera, 2023",
+      url:
+        "https://github.com/atom-loves-you/main/blob/main/mp3/143%20Nao.mp3",
+      cover_art_url: "https://assets.codepen.io/6566924/bright-colors-grid-square.png"
+    },
+    {
+      name: "Track 09: Last Track",
+      artist: "Atom Loves You",
+      album: "Ad Astra per Aspera, 2023",
+      url:
+        "https://github.com/atom-loves-you/main/blob/main/mp3/Last%20track.mp3",
       cover_art_url: "https://assets.codepen.io/6566924/bright-colors-grid-square.png"
     }
   ]
 });
+
+
+
 
 window.onkeydown = function (e) {
   return !(e.keyCode == 32);
